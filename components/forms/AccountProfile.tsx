9869cffcecfedb0c975f30607ae89b91eb40166d
Muhammad Usman <muusman479@gmail.com>
2023-09-15 03:34:32 +0500

    topbar,sidebar,footer,rightsidebar & onboarding page added

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+
+interface Props {
+    user: {
+        id: string,
+        objectId: string,
+        username: string,
+        name: string,
+        bio: string,
+        image: string,
+    };
+    btnTitle: string
+}
+
+export default function AccountProfile({ user, btnTitle }: Props) {
+    return (
+        <div>
+            AccountProfile
+        </div>
+    )
+}
